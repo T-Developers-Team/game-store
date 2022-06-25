05f1d586545f72b1b406b4bc32170dc7c609c55a
Galeid <galeid475@gmail.com>
2022-06-25 11:52:43 -0500

    Creating Header component

--- src/components/layout/footer/index.jsx
+++ src/components/layout/footer/index.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import placeholderImgUrl from '../../../assets/logo-placeholder.png'
+import {AiFillFacebook, AiFillTwitterSquare} from 'react-icons/ai'
 
 const FooterComponent = () => {
   return (
-    <div>FooterComponent</div>
+    <footer className='bg-zinc-800 text-gray-400 text-sm'>
+      <div className='max-w-5xl mx-auto py-10'>
+        <hr className='mb-2 mt-1 border-gray-600'/>
+        <div className='flex items-center gap-5'>
+          <figure className=''>
+            <img src={placeholderImgUrl}/>
+          </figure>
+          <div className=''>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida arcu non urna consequat lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam interdum eros ac eros lobortis convallis. Pellentesque porttitor libero vitae ultricies vulputate. Aliquam hendrerit convallis ultricies. In vehicula volutpat posuere. Nam maximus auctor elit, sit amet ullamcorper nisi imperdiet et.
+          </div>
+        </div>
+        <hr className='my-2 border-gray-600'/>
+        <div className='flex gap-2.5 text-gray-600'>
+          <a href='#' className='hover:text-gray-300 text-gray-400'>About Us</a> |
+          <a href='#' className='hover:text-gray-300 text-gray-400'>Jobs</a> |
+          <a href='#' className='hover:text-gray-300 text-gray-400'>Launcher Distribution</a> |
+          <a href='#' className='hover:text-gray-300 text-gray-400'>Support</a> |
+          <a href='#' className='hover:text-gray-300 text-gray-400 flex items-center'><AiFillFacebook/>Facebook</a> |
+          <a href='#' className='hover:text-gray-300 text-gray-400 flex items-center'><AiFillTwitterSquare/>Twitter</a>
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- src/components/layout/header/index.jsx
+++ src/components/layout/header/index.jsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import React, {Fragment } from 'react'
+import placeholderImgUrl from '../../../assets/logo-placeholder.png'
+import { FaGamepad } from 'react-icons/fa'
+import { MdArrowDropDown } from 'react-icons/md'
+import { Menu, Transition } from '@headlessui/react';
 
 const HeaderComponent = () => {
   return (
-    <div>HeaderComponent</div>
+    <header className='bg-zinc-800 text-gray-400 text-xs'>
+      <div className="max-w-5xl mx-auto flex">
+        <figure className="px-8 py-4">
+          <img src={placeholderImgUrl} className="max-h-20"/>
+        </figure>
+        <div className="grow">
+          <section className='flex gap-3 justify-end items-center py-2'>
+            <button className="bg-lime-700 text-gray-200 py-1 px-2 hover:text-white flex items-center"><FaGamepad className='text-base' /> <p className='pl-1.5'>Install Launcher</p></button>
+            <div className='hover:text-gray-300'>Login</div>
+            |
+            <Menu as="div" className="relative inline-block text-left">
+              {({ open }) => (
+                <>
+                  <div>
+                    <Menu.Button className="hover:text-gray-300 flex items-center">
+                      Language
+                      <MdArrowDropDown
+                        className="text-xl mt-0.5"
+                      />
+                    </Menu.Button>
+                  </div>
+
+                  {/* Items to render when button is clicked */}
+                  <Transition
+                    as={Fragment}
+                    show={open}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="bg-zinc-900 absolute right-0 w-56 mt-2 origin-top-right divide-y divide-gray-300 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <div className="px-1 py-1">
+                        <Menu.Item>
+                          <p>Spanish</p>
+                        </Menu.Item>
+                      </div>
+
+                      <div className="px-1 py-1">
+                        <Menu.Item>
+                        <p>English</p>
+                        </Menu.Item>
+                      </div>
+                    </Menu.Items>
+                  </Transition>
+                </>
+              )}
+            </Menu>
+          </section>
+          <section className='flex gap-3 items-center pt-2'>
+            <nav>
+              <ul className='flex text-sm uppercase gap-4 font-medium'>
+                <li className='hover:text-gray-300'>Store</li>
+                <li className='hover:text-gray-300'>Community</li>
+                <li className='hover:text-gray-300'>About</li>
+                <li className='hover:text-gray-300'>Support</li>
+              </ul>
+            </nav>
+          </section>
+        </div>
+      </div>
+    </header>
   )
 }
 
